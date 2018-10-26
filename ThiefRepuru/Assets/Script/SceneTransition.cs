34ae1151514f8bfb5cc489eb122365f9e7fd259a
DESKTOP-I70F9GA\raiwe <sdsjhsgd@gmail.com>
2018-10-26 22:36:26 +0900

    ①GameSceneの修正、②シーン遷移するためのスクリプト用意、③各シーンにボタン配置
    ①GameOver、GameClearシーンを→Resultシーンに修正。
    ②シーン遷移するための関数用意。
    ③ボタンを押したら各シーンに遷移するようにした。

---
+++ ThiefRepuru/Assets/Script/SceneTransition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneTransition : MonoBehaviour {
+
+    //TitleSceneに遷移する関数-----------------------------
+    public void TitleTrasition() {
+        SceneManager.LoadScene("Title");
+    }
+    //-----------------------------------------------------
+
+    //StageSelectSceneに遷移する関数-----------------------
+    public void StageSelectTrasition() {
+        SceneManager.LoadScene("StageSelect");
+    }
+    //-----------------------------------------------------
+
+    //MainSceneに遷移する関数-----------------------------
+    public void MainTrasition() {
+        SceneManager.LoadScene("Main");
+    }
+    //-----------------------------------------------------
+
+    //ResultSceneに遷移する関数----------------------------
+    public void ResultTrasition() {
+        SceneManager.LoadScene("Result");
+    }
+    //-----------------------------------------------------
+                                                                      
+}
