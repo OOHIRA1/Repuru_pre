a84d3c1859ac4c7452f94517e2332a4e4fa251ea
yuzawa <a@gmail.com>
2018-11-15 12:21:28 +0900

    Itemのスクリプト
    Itemスクリプトの追加、Itemの判定を付けた

---
+++ ThiefRepuru/Assets/Script/ItemGet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGet : MonoBehaviour {
+    [SerializeField] GameObject ItemDetails = null;　//Itemの詳細
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //アイテム詳細の表示をする関数--------------------
+    public void ItemDisplay( bool ItemValue ) {
+        ItemDetails.SetActive( ItemValue );
+    }
+    //------------------------------------------------
+
+}
